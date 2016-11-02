b79defe647c489ac8a7b23ae872a077ff14ad2af
Mithos <ShiraLio@outlook.fr>
2016-11-02 14:31:37 +0100

    added test with @Test...

--- src/test/java/net/apispark/webapi/db/ContactPersistenceTest.java
+++ src/test/java/net/apispark/webapi/db/ContactPersistenceTest.java
@@ -1,9 +1,11 @@
 package net.apispark.webapi.db;
 
 import org.junit.Assert;
+import org.junit.Test;
 
 public class ContactPersistenceTest {
 	
+	@Test
 	public void TestUnitaire() {	
 		Assert.assertEquals(true, false) ;
 	}
